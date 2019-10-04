32879eb5c10d300f84bc67795e409fb7ee3734de
Adit Gupta <aditgupta1@yahoo.com>
2019-10-04 12:18:15 -0400

    new changes

--- js/javascript.js
+++ js/javascript.js
@@ -21,14 +21,13 @@ function readURL(input, imgId) {
 }
 
 $(document).ready(function () {
-    //@naresh action dynamic childs
     var next = 0;
     $("#add-more").click(function(e){
         e.preventDefault();
         var addto = "#field" + next;
         var addRemove = "#field" + (next);
         next = next + 1;
-        var newIn = ' <div id="field'+ next +'" name="field'+ next +'"><div id="field0"><div class="form-group"> <label class="col-md-4 control-label" for="action_id">Name</label><div class="col-md-5"> <input id="action_id" name="action_id" type="text" placeholder="" class="form-control input-md"></div></div><div class="form-group"> <label class="col-md-4 control-label" for="action_name">Email</label><div class="col-md-5"> <input id="action_name" name="action_name" type="text" placeholder="" class="form-control input-md"></div></div></div></div>';
+        var newIn = ' <div id="field'+ next +'" name="field'+ next +'"><!-- Text input--><div class="form-group"> <label class="col-md-4 control-label" for="user_name">Name</label> <div class="col-md-5"> <input id="user_name" name="user_name" type="text" placeholder="" class="form-control input-md"> </div></div> <!-- Text input--><div class="form-group"> <label class="col-md-4 control-label" for="user_email">Email</label> <div class="col-md-5"> <input id="user_email" name="user_email" type="text" placeholder="" class="form-control input-md"> </div></div> <!-- File Button --> <div class="form-group"> <label class="col-md-4 control-label" for="user_picture">Member Photo</label> <div class="col-md-4"> <input id="user_picture" name="user_picture" class="input-file" type="file"> </div></div></div>';
         var newInput = $(newIn);
         var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >Remove</button></div></div><div id="field">';
         var removeButton = $(removeBtn);
@@ -47,3 +46,12 @@ $(document).ready(function () {
     });
 
 });
+
+
+function downloadContent(name, content) {
+    var atag = document.createElement("a");
+    var file = new Blob([""], {type: 'image/png'});
+    atag.href = URL.createObjectURL(file);
+    atag.download = name;
+    atag.click();
+  }
