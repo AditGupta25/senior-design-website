dcffb51cc39889d0a855d4e5044ed3faa11ef5d3
Adit Gupta <aditgupta1@yahoo.com>
2019-10-06 00:56:53 -0400

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,55 @@
+var globalInformation = {
+    projectName: "",
+    oneLineProjectDesc : "",
+    teamLogo : "",
+    abstract : "",
+    screenshot1: "",
+    screenshot2: "",
+    teamMemberPhotos: {},
+    teamMemberNames: [],
+    teamMemberEmails: []
+
+}
+
+
+
 function readURL(input, imgId) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            console.log(e)
+            // console.log(e.target.result)
             if(imgId == "companyLogo"){
                 $('#' + imgId)
                 .attr('src', e.target.result)
                 .width(500)
                 .height(500);
+
+                globalInformation.teamLogo = e.target.result;
             }
             if(imgId == "screenshot1" || imgId == "screenshot2"){
                 $('#' + imgId)
                 .attr('src', e.target.result)
                 .width(500)
                 .height(500);
+
+                if(imgId == "screenshot1"){
+                    globalInformation.screenshot1 = e.target.result;
+                }else{
+                    globalInformation.screenshot2 = e.target.result;
+                }
             }
             else{
-                console.log(imgId)
                 $('#' + imgId)
                 .attr('src', e.target.result)
                 .width(250)
                 .height(250);
+
+                globalInformation.teamMemberPhotos[imgId] = e.target.result
             }
         };
 
         reader.readAsDataURL(input.files[0]);
-        console.log(input.files[0])
 
     }
 }
@@ -77,6 +100,8 @@ function validateInputs(){
     if(projName == ""){
         alert("Please enter a Project name!")
         validForm=false;
+    }else{
+        globalInformation.projectName = projName;
     }
 
     //Check One line description
@@ -84,6 +109,8 @@ function validateInputs(){
     if(projectDescription ==""){
         alert("Please enter a Project description!")
         validForm=false;
+    }else{
+        globalInformation.oneLineProjectDesc = projectDescription;
     }
 
     //Check Abstract
@@ -91,35 +118,100 @@ function validateInputs(){
     if(abstract == ""){
         alert("Please enter a valid Abstract!")
         validForm=false;
+    }else{
+        globalInformation.abstract = abstract;
     }
 
     // validate Images
-
     // logo
-    var logo_x = document.getElementById("myImg").naturalWidth;
-    var logo_y = document.getElementById("myImg").naturalHeight;
+    var logo_x = document.getElementById("companyLogo").naturalWidth;
+    var logo_y = document.getElementById("companyLogo").naturalHeight;
+    if(logo_x != 500 && logo_y != 500){
+        console.log(logo_x, logo_y)
+        alert("Your Logo is: " + logo_x + " x " +logo_y + ". Please make sure Logo is 500px by 500px exactly!")
+        validForm=false;
+    }
 
     //screenshot1
-    var screenshot1_x = document.getElementById("myImg").naturalWidth;
-    var screenshot1_y = document.getElementById("myImg").naturalHeight;
+    var screenshot1_x = document.getElementById("screenshot1").naturalWidth;
+    var screenshot1_y = document.getElementById("screenshot1").naturalHeight;
+    if(screenshot1_x != 500 && screenshot1_y != 500){
+        console.log(screenshot1_x, screenshot1_y)
+        alert("Your Screenshot 1 is: " + screenshot1_x + " x " + screenshot1_y + ". Please make sure Screenshot 1 is 500px by 500px exactly!")
+        validForm=false;
+    }
 
     //screenshot2
-    var screenshot2_x = document.getElementById("myImg").naturalWidth;
-    var screenshot2_y = document.getElementById("myImg").naturalHeight;
-
-
-    // Validate Team Members
-
-
-
-    // Validate Images
-
+    var screenshot2_x = document.getElementById("screenshot2").naturalWidth;
+    var screenshot2_y = document.getElementById("screenshot2").naturalHeight;
+    if(screenshot2_x != 500 && screenshot2_y != 500){
+        console.log(screenshot2_x, screenshot2_y)
+        alert("Your Screenshot 2 is: " + screenshot2_x + " x " +screenshot2_y + ". Please make sure Screenshot 2 is 500px by 500px exactly!")
+        validForm=false;
+    }
     return validForm;
+}
+
+function getUserAttributes(){
+        var userAttributeErrorFound = false;
+
+        var userNamesInput = $( "input[name*='user_name']" );
+        var userNames = [];
+        for(var i=0; i<userNamesInput.length; i++){
+            // console.log(userNamesInput[i].value)
+            if(userNamesInput[i].value == ""){
+                userAttributeErrorFound = true;
+            }else{
+                userNames.push(userNamesInput[i].value) 
+            }
+        }
+    
+        var userEmailInput = $( "input[name*='user_email']" );
+        var userEmails = [];
+        for(var i=0; i<userEmailInput.length; i++){
+            // console.log(userEmailInput[i].value)
+            if(userEmailInput[i].value == ""){
+                userAttributeErrorFound = true;
+            }else{
+                userEmails.push(userEmailInput[i].value) 
+            }
+        }
+
+        if(userAttributeErrorFound == false || userEmails.length != userNames.length){
+            alert("Please make sure all member names and emails are filled out!")
+            return("error");
+        }else{
+            // setting global variables
+            globalInformation.teamMemberNames = userNames;
+            globalInformation.teamMemberEmails = userEmails;
+        }
 }
 
 function submitForm(){
     //Validate inputs
     if(this.validateInputs()){
-        alert("WORKD")
+        var userAttributes = this.getUserAttributes();
+        if(userAttributes != "error"){
+            console.log("errorreddd")
+        }else{
+            console.log(globalInformation)
+        }
     }
+
+    // var userAttributes = this.getUserAttributes();
+    // if(userAttributes != "error"){
+    //     console.log("errorreddd")
+    // }else{
+    //     console.log(globalInformation)
+    // }
 }
+
+
+
+// Things to do still: 
+// 1) Validate user images
+// 2) See why user image is not coming on image for new users 
+// 3) Create basic website template 
+// 4) fill in things from global information variable to make website personalized
+// 5) download images and html file
+// 6) Congratulations you have downloaded all the items, make sure you zip them
